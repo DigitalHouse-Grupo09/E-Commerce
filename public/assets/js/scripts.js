c31ca342b86cf44ad9a8e83389fee7b06f477071
Milo Molina <comodinx@gmail.com>
2021-11-29 15:32:32 -0300

    Add banner with animation

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,1 +1,58 @@
-scripts.js
+
+/**
+ * APPLICATION
+ */
+(function (window) {
+    window.app = window.app || {};
+})(window);
+
+/**
+ * BANNERS
+ */
+(function (window) {
+    let slideIndex = 1;
+
+    // Next/previous controls
+    function changeSlides (n) {
+        showSlides(slideIndex += n);
+    }
+
+    // Thumbnail image controls
+    function currentSlide (n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides (n) {
+        let slides = document.getElementsByClassName('banner');
+        let dots = document.getElementsByClassName('banner-dot');
+        let i;
+
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = 'none';
+        }
+
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(' active', '');
+        }
+
+        slides[slideIndex-1].style.display = 'block';
+        dots[slideIndex-1].className += ' active';
+    }
+
+    // Initialize banner
+    showSlides(slideIndex);
+
+    // Add slider to app
+    window.app = window.app || {};
+    window.app.banner = window.app.banner || {};
+    window.app.banner.changeSlides = changeSlides;
+    window.app.banner.currentSlide = currentSlide;
+    window.app.banner.showSlides = showSlides;
+})(window);
