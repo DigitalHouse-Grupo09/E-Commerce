dced00d87d6b02d5329db9580f0ce73f1b32bee3
Milo Molina <comodinx@gmail.com>
2021-11-29 15:59:50 -0300

    Banner style

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -10,27 +10,34 @@
  * BANNERS
  */
 (function (window) {
+    const bannerTime = 5000;
     let slideIndex = 1;
+    let slideTimeout;
 
-    // Next/previous controls
-    function changeSlides (n) {
-        showSlides(slideIndex += n);
+
+    /**
+     * @param  {number}  direction  1 for next, -1 for previous.
+     */
+    function changeSlides (direction) {
+        showSlides(slideIndex += direction);
     }
 
-    // Thumbnail image controls
-    function currentSlide (n) {
-        showSlides(slideIndex = n);
+    /**
+     * @param  {number}  slide  Slide index.
+     */
+    function currentSlide (slide) {
+        showSlides(slideIndex = slide);
     }
 
-    function showSlides (n) {
+    function showSlides (slide) {
         let slides = document.getElementsByClassName('banner');
         let dots = document.getElementsByClassName('banner-dot');
         let i;
 
-        if (n > slides.length) {
+        if (slide > slides.length) {
             slideIndex = 1;
         }
-        if (n < 1) {
+        if (slide < 1) {
             slideIndex = slides.length;
         }
 
@@ -42,8 +49,14 @@
             dots[i].className = dots[i].className.replace(' active', '');
         }
 
-        slides[slideIndex-1].style.display = 'block';
-        dots[slideIndex-1].className += ' active';
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].className += ' active';
+
+        // Auto change slide
+        if (slideTimeout) {
+            clearTimeout(slideTimeout);
+        }
+        slideTimeout = setTimeout(() => changeSlides(1), bannerTime);
     }
 
     // Initialize banner
