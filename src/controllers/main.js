f85de575a5e2a31b96b599142685b5d6b3c662f2
cristianebes <cristian.besada@hotmail.com>
2022-03-13 22:02:03 -0300

    Merge

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -9,8 +9,13 @@ const { products } = require('../models');
 // home
 const home = (req, res) => res.render('home', { products: products.getAll().sort(() => .5 - Math.random()) });
 const help = (req, res) => res.render('home/help');
+const regretful = (req, res) => res.render('home/regretful');
+const contact = (req, res) => res.render('home/contact');
+const privacy = (req, res) => res.render('home/privacy');
+
+
 
 //
 // export
 //
-module.exports = { home, help };
+module.exports = { home, help, regretful, contact, privacy };
--- src/routes/main.js
+++ src/routes/main.js
@@ -10,6 +10,12 @@ const controller = require('../controllers/main');
 //
 router.get('/', controller.home);
 router.get('/help', controller.help);
+router.get('/regretful', controller.regretful);
+router.get('/contact', controller.contact);
+router.get('/privacy', controller.privacy);
+
+
+
 
 //
 // export
