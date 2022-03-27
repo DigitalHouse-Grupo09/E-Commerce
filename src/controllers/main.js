992971f2640b74f890949d100e6d4cff040657a9
Milo Molina <comodinx@gmail.com>
2022-03-26 22:43:21 -0300

    Fixed search books

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -4,30 +4,25 @@
 const { products } = require('../models');
 
 //
+// constants
+//
+const regexpSpaces = /\\s+/g;
+
+//
 // endpoints
 //
 // home
 const home = (req, res) => res.render('home', { products: products.getAll().sort(() => .5 - Math.random()) });
 
-
-// search books
+// search
 const search = (req, res) => {
+	const search = (req.body.search || '').trim().toLowerCase().replace(regexpSpaces, '*');
+	const filtered = products.filter(search);
 
-	const searchElement = req.body.search.toLowerCase();
-
-	const searchProducts = []
-
-	products.getAll().map(product => {
-		if(product.title.toLowerCase().includes(searchElement)) {
-			return searchProducts.push(product)
-		}
-	})
-
-	if(searchProducts.length > 0) {
-		res.render('home/results', { products: searchProducts })
-	} else {
-		res.render('404')
-	}
+	res.render('home/search', {
+		products: filtered,
+		search
+	});
 }
 
 const help = (req, res) => res.render('home/help');
@@ -35,9 +30,6 @@ const regretful = (req, res) => res.render('home/regretful');
 const contact = (req, res) => res.render('home/contact');
 const privacy = (req, res) => res.render('home/privacy');
 const test = (req, res) => res.render('home/test');
-
-
-
 
 //
 // export
